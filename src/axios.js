588467699031fa84573db4fac08ebde63e797c7f
Hamza Ali Khan <drstoneeren@gmail.com>
2022-09-27 07:38:01 -0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
-  //   baseURL: "https://agile-chamber-11438.herokuapp.com",
+  // baseURL: "http://localhost:9000",
+  baseURL: "https://agile-chamber-11438.herokuapp.com",
 });
 
 export default instance;
